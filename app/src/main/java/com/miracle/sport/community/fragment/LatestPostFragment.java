b3ba3e5b5387dc0f19468f6effc8724b4eac7370
michael <952419245@qq.com>
2018-11-10 17:01:32 +0800

    Michael

--- app/src/main/java/com/miracle/sport/community/fragment/LatestPostFragment.java
+++ app/src/main/java/com/miracle/sport/community/fragment/LatestPostFragment.java
@@ -52,6 +52,7 @@ public class LatestPostFragment extends BaseFragment<FragmentHotpostBinding> {
         callBack = new ZPageLoadCallback<ZResponse<List<PostBean>>>(mAdapter, binding.recyclerView) {
             @Override
             public void requestAction(int page, int pageSize) {
+                callBack.setCachKey("LatestPostFragment" + circleId);
                 if (isCommunityActivity) {
                     ZClient.getService(SportService.class).getPostList(null, circleId, page, pageSize).enqueue(this);
                 } else {
@@ -59,8 +60,6 @@ public class LatestPostFragment extends BaseFragment<FragmentHotpostBinding> {
                 }
             }
         };
-        callBack.setCachKey("LatestPostFragment");
-
         if (isCommunityActivity) {
             callBack.setSwipeRefreshLayout(((CommunityActivity) getActivity()).getSwipeRefreshLayout());
         } else {
